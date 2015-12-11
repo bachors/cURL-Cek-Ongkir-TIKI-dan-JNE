53e4d8547da81e1b355a4098b0b603d5ebd2f232
Bachors <bachor.can@gmail.com>
2015-12-11 19:52:15 +0700

    demo

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<a href="https://github.com/bachors/cURL-Cek-Ongkir-TIKI-dan-JNE" target="_BLANK"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
+
 <form method="GET">
 	<select name="dari">
 		<option value="bandung">Bandung</option>
@@ -23,6 +25,7 @@
 
 <?php
 
+	// Memanggil fungsi
 	require("ongkir.php");
 
 	if(!empty($_GET['service'])&&!empty($_GET['dari'])&&!empty($_GET['ke'])&&!empty($_GET['berat'])){
@@ -32,8 +35,10 @@
 		$kg = $_GET['berat'];
 		$user_agent = "Googlebot/2.1 (http://www.googlebot.com/bot.html)";
 		if($service == 'tiki'){
+			// menampilkan ongkir TIKI
 			echo "<p>Dari: $dari<br>Ke: $ke<br>Berat (Kg): $kg</p>".tiki($dari,$ke,$kg,$user_agent);
 		}else if($service == 'jne'){
+			// menampilkan ongkir JNE
 			echo kota($dari,$ke,$kg,$user_agent);
 		}
 	}
